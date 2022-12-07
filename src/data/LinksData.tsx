d328504eed978e6e67806c9bb4b43665537de17e
dhkim4 <dhkim4@seegene.com>
2022-12-07 19:41:37 +0900

    edit links

--- src/data/LinksData.tsx
+++ src/data/LinksData.tsx
@@ -1,7 +1,6 @@
 import { ImGithub, ImLinkedin } from "react-icons/im";
-import { FaTwitter } from "react-icons/fa";
-import { SiInstagram } from "react-icons/si";
-
+import { SiLeetcode } from "react-icons/si";
+import { BsJournals } from "react-icons/bs";
 interface LinksType {
   title: string;
   icon: any;
@@ -12,21 +11,21 @@ export const LinksData: LinksType[] = [
   {
     title: "GitHub",
     icon: <ImGithub color='#000000dc' />,
-    link: "https://github.com/erfjs",
+    link: "https://github.com/ampersandor",
   },
   {
     title: "LinkedIn",
     icon: <ImLinkedin color='#000000dc' />,
-    link: "https://www.linkedin.com/in/erfan-granmayeh",
+    link: "https://www.linkedin.com/in/dong-hun-kim-4a3119168/",
   },
   {
-    title: "Twitter",
-    icon: <FaTwitter color='#000000dc' />,
-    link: "https://twitter.com/erfan_grr",
+    title: "Blog",
+    icon: <BsJournals color='#000000dc' />,
+    link: "https://ampersandor.tistory.com/",
   },
   {
-    title: "Instagram",
-    icon: <SiInstagram color='#000000dc' />,
-    link: "https://www.instagram.com/erfan_grr",
+    title: "LeetCode",
+    icon: <SiLeetcode color='#000000dc' />,
+    link: "https://leetcode.com/williamkim/",
   },
 ];
