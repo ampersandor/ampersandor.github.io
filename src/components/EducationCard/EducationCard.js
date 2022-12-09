581f7f9698d285afc143f0f14f62829687ec27b7
DongHun Kim <donghunkim@DongHuns-MacBook-Air.local>
2022-12-09 15:56:43 +0900

    add icon

--- src/components/EducationCard/EducationCard.js
+++ src/components/EducationCard/EducationCard.js
@@ -1,12 +1,27 @@
 import { IoIosSchool } from 'react-icons/io';
+import { GiMachineGunMagazine} from 'react-icons/gi';
+import { FaBuilding } from 'react-icons/fa';
 
 import styles from './educationCard.module.scss';
+
+function judgeIcon(title){
+  if(title.toLowerCase().includes("university") || title.toLowerCase().includes("school")){
+    return <IoIosSchool className={styles.icon}/>;
+  }
+  else if(title.toLowerCase().includes("military")){
+    return <GiMachineGunMagazine className={styles.icon}/>
+  }
+  else{
+    return <FaBuilding className={styles.icon}/>;
+  }
+}
 
 const EducationCard = (props) => {
   return (
     <div className={`${styles.row} ${props.isLeft ? styles.row_1 : styles.row_2}`}>
       <section>
-        <IoIosSchool className={styles.icon} />
+        {/* <IoIosSchool className={styles.icon} /> */}
+        {judgeIcon(props.title)}
         <div className={styles.details}>
           <span className={styles.title}>{props.title}</span>
           <span>{props.date}</span>
